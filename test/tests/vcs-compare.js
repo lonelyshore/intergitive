0f06f656ffcf38392f0b098ec86dd41abf8b5508
zedaizd <zedaizd@gmail.com>
2019-04-21 10:35:42 +0800

    Test for VCS comparing functionaliry (WIP)

---
+++ test/tests/vcs-compare.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const path = require("path");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
+
+describe.skip("VCS Compare", function() {
+    describe("Local", function() {
+        describe("Equal", function() {
+            it("clean stage and working directory", function() {
+    
+            });
+    
+            it("dirty stage", function() {
+    
+            });
+    
+            it("dirty working directory", function() {
+    
+            });
+    
+            it("merged", function() {
+    
+            });
+    
+            it("merging", function() {
+    
+            });
+    
+            it("merging conflict", function() {
+    
+            });
+    
+            it("merging conflict editted", function() {
+    
+            });
+    
+            it("merging conflict resolved", function() {
+    
+            });
+    
+            it("detached head", function() {
+    
+            });
+        });
+    
+        describe("Different", function() {
+    
+        });
+    });
+});
