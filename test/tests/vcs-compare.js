5c5643910a4afa4585ee6b09d13835d6092e5800
zedaizd <zedaizd@gmail.com>
2019-04-21 11:36:52 +0800

    Test for VCS comparing (WIP)

--- test/tests/vcs-compare.js
+++ test/tests/vcs-compare.js
@@ -11,39 +11,53 @@ describe.skip("VCS Compare", function() {
     describe("Local", function() {
         describe("Equal", function() {
             it("clean stage and working directory", function() {
-    
+                const referenceName = "clean";
+
             });
     
             it("dirty stage", function() {
-    
+                const referenceName = "dirty-stage";
+
             });
     
             it("dirty working directory", function() {
-    
+                const referenceName = "dirty-directory";
+                
             });
     
             it("merged", function() {
-    
+                const referenceName = "merged";
+
             });
     
             it("merging", function() {
-    
+                const referenceName = "merging";
+
             });
     
             it("merging conflict", function() {
-    
+                const referenceName = "conflict";
+
             });
     
             it("merging conflict editted", function() {
-    
+                const referenceName = "conflict-edit";
+                
+            });
+
+            it("merging conflict staged", function() {
+                const referenceName = "conflict-staged";
+
             });
     
             it("merging conflict resolved", function() {
-    
+                const referenceName = "conflict-resolved";
+                
             });
     
             it("detached head", function() {
-    
+                const referenceName = "detached";
+                
             });
         });
     
