b2c9669dd5753c4d8ee75d9854c723cf7f4b5169
zedaizd <zedaizd@gmail.com>
2019-05-26 21:06:33 +0800

    Execute repo generation script in renderer process

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadFile('index.html')
+  win.webContents.openDevTools();
 }
 
 app.on('ready', createWindow)
---
+++ test.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+const fs = require("fs-extra");
+const path = require("path");
+
+const resoruceBasePath = path.resolve("./test/resources");
+const assetStorePath = path.join(resoruceBasePath, "vcs-compare", "assets");
+const yamlSubPath = path.join("vcs-compare", "generate-base-repo.yaml");
+
+const workingPath = path.resolve("./test/playground/generate-vcs-repo");
+
+const nodeConsole = require('console');
+let myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+myConsole.log(workingPath);
+
+let initializeRepo = (sourceRepoPath) => {
+    return fs.emptyDir(sourceRepoPath);
+}
+
+let gen = require("./dev/generate-base-repo");
+
+myConsole.log(gen)
+gen.generateBaseRepo(
+    workingPath,
+    assetStorePath,
+    path.join(resoruceBasePath, yamlSubPath),
+    initializeRepo
+);
