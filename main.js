39df82ee3f26e45a95f06f7ba0ec72cbeb8e69a6
zedaizd <zedaizd@gmail.com>
2019-05-26 11:44:27 +0800

    Add quick start main page to test electron

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
+// const fs = require("fs-extra");
+// const path = require("path");
+
+// const resoruceBasePath = path.resolve(__dirname, "../resources");
+// const assetStorePath = path.join(resoruceBasePath, "vcs-compare", "assets");
+// const yamlSubPath = path.join("vcs-compare", "generate-base-repo.yaml");
+
+// const workingPath = path.resolve(__dirname, "../playground/generate-vcs-repo");
+
+// let initializeRepo = (sourceRepoPath) => {
+//     return fs.emptyDir(sourceRepoPath);
+// }
+
+// require("../../dev/generate-base-repo").generateBaseRepo(
+//     workingPath,
+//     assetStorePath,
+//     path.join(resoruceBasePath, yamlSubPath),
+//     initializeRepo
+// );
