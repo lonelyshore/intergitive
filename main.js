3abc6595d2c5f30e789bb25dbccaf55b653a7383
zedaizd <test@test.server>
2019-06-10 09:02:37 +0800

    Switch index backing script to render-level for development

--- main.js
+++ main.js
@@ -18,22 +18,4 @@ function createWindow () {
 }
 
 app.on('ready', createWindow)
-// const fs = require("fs-extra");
-// const path = require("path");
 
-// const resoruceBasePath = path.resolve(__dirname, "../resources");
-// const assetStorePath = path.join(resoruceBasePath, "vcs-compare", "assets");
-// const yamlSubPath = path.join("vcs-compare", "generate-base-repo.yaml");
-
-// const workingPath = path.resolve(__dirname, "../playground/generate-vcs-repo");
-
-// let initializeRepo = (sourceRepoPath) => {
-//     return fs.emptyDir(sourceRepoPath);
-// }
-
-// require("../../dev/generate-base-repo").generateBaseRepo(
-//     workingPath,
-//     assetStorePath,
-//     path.join(resoruceBasePath, yamlSubPath),
-//     initializeRepo
-// );
