b060836ffa16ea890b0cbfefbde536e09704569e
zedaizd <test@test.server>
2019-06-15 21:55:10 +0800

    Add component for elaborate step (WIP)

---
+++ paths.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const runAndTestBasePath = path.resolve(__dirname, './playground');
+
+const runAndTest = {
+    RESOURCES_PATH: path.join(runAndTestBasePath, "resources"),
+    BUNDLE_PATH: [],
+};
+
+const result = runAndTest;
+
+module.exports.RESOURCES_PATH = result.RESOURCES_PATH;
+module.exports.BUNDLE_PATH = rsult.BUNDLE_PATH;
