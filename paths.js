bb00b02b93512898ccd0ba92c9f938b0ac544fb2
zedaizd <test@test.server>
2019-06-19 22:10:25 +0800

    Replace mustache with infile assets

--- paths.js
+++ paths.js
@@ -4,9 +4,11 @@ const runAndTestBasePath = path.resolve(__dirname, './example');
 const runAndTest = {
     RESOURCES_PATH: path.join(runAndTestBasePath, "resources"),
     BUNDLE_PATH: [],
+    STATIC_PATH: path.join(path.relative(RESOURCES_PATH, __dirname), 'static'),
 };
 
 const result = runAndTest;
 
 module.exports.RESOURCES_PATH = result.RESOURCES_PATH;
 module.exports.BUNDLE_PATH = result.BUNDLE_PATH;
+module.exports.STATIC_PATH = result.STATIC_PATH;
