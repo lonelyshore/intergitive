599acaef64ab73367c5d94070bf3eade521ddb1c
zedaizd <test@test.server>
2019-06-25 22:32:42 +0800

    Dev schema uses tags from production schema to ensure production schema can parse dev only contents

--- dev/config-schema.js
+++ dev/config-schema.js
@@ -2,7 +2,9 @@
 
 const yaml = require("js-yaml");
 const actionConfig = require("./config-action");
+const dummyActionConfigDict = require('../lib/dummy-dev-action-config-schema').devActionSchemaDict;
 const upstream = require("../lib/level-config-schema");
+
 
 const isString = function(obj) {
     return typeof obj === 'string' || obj instanceof String;
@@ -15,126 +17,144 @@ const memberIsStringArray = function(data, memberName) {
         && data[memberName].all(e => isString(e));
 }
 
-let unstageActionType = new yaml.Type("!unstage", {
-    kind: "mapping",
+let unstageActionType = new yaml.Type(
+    dummyActionConfigDict.unstage.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && data instanceof Object
-            && memberIsStringArray("pathSpecs")
-            && "repoSetupName" in data
-            && isString(data.repoSetupName);
-    },
+        resolve: function(data) {
+            return data !== null
+                && data instanceof Object
+                && memberIsStringArray("pathSpecs")
+                && "repoSetupName" in data
+                && isString(data.repoSetupName);
+        },
 
-    construct: function(data) {
-        return new actionConfig.UnstageAction(
-            data.repoSetupName,
-            data.pathSpecs
-        );
-    },
+        construct: function(data) {
+            return new actionConfig.UnstageAction(
+                data.repoSetupName,
+                data.pathSpecs
+            );
+        },
 
-    instanceOf: actionConfig.UnstageAction
-});
+        instanceOf: actionConfig.UnstageAction
+    }
+);
 
-let unstageAllActionType = new yaml.Type("!unstageAll", {
-    kind: "mapping",
+let unstageAllActionType = new yaml.Type(
+    dummyActionConfigDict.unstageAll.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && data instanceof Object
-            && "repoSetupName" in data
-            && isString(data.repoSetupName);
-    },
+        resolve: function(data) {
+            return data !== null
+                && data instanceof Object
+                && "repoSetupName" in data
+                && isString(data.repoSetupName);
+        },
 
-    construct: function(data) {
-        return new actionConfig.UnstageAllAction(data.repoSetupName);
-    },
+        construct: function(data) {
+            return new actionConfig.UnstageAllAction(data.repoSetupName);
+        },
 
-    instanceOf: actionConfig.UnstageAllAction
-});
+        instanceOf: actionConfig.UnstageAllAction
+    }
+);
 
-let mergeActionType = new yaml.Type("!merge", {
-    kind: "mapping",
+let mergeActionType = new yaml.Type(
+    dummyActionConfigDict.merge.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && data instanceof Object
-            && "repoSetupName" in data
-            && isString(data.repoSetupName)
-            && "withBranch" in data
-            && isString(data.withBranch);
-    },
+        resolve: function(data) {
+            return data !== null
+                && data instanceof Object
+                && "repoSetupName" in data
+                && isString(data.repoSetupName)
+                && "withBranch" in data
+                && isString(data.withBranch);
+        },
 
-    construct: function(data) {
-        return new actionConfig.MergeAction(
-            data.repoSetupName,
-            data.withBranch
-        );
-    },
+        construct: function(data) {
+            return new actionConfig.MergeAction(
+                data.repoSetupName,
+                data.withBranch
+            );
+        },
 
-    instanceOf: actionConfig.MergeAction
-});
+        instanceOf: actionConfig.MergeAction
+    }
+);
 
-let continueMergeActionType = new yaml.Type("!continueMerge", {
-    kind: "mapping",
+let continueMergeActionType = new yaml.Type(
+    dummyActionConfigDict.continueMerge.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && "repoSetupName" in data
-            && isString(data.repoSetupName);
-    },
+        resolve: function(data) {
+            return data !== null
+                && "repoSetupName" in data
+                && isString(data.repoSetupName);
+        },
 
-    construct: function(data) {
-        return new actionConfig.ContinueMergeAction(
-            data.repoSetupName
-        );
-    },
+        construct: function(data) {
+            return new actionConfig.ContinueMergeAction(
+                data.repoSetupName
+            );
+        },
 
-    instanceOf: actionConfig.ContinueMergeAction
-});
+        instanceOf: actionConfig.ContinueMergeAction
+    }
+);
 
-let cleanCheckoutActionType = new yaml.Type("!cleanCheckout", {
-    kind: "mapping",
+let cleanCheckoutActionType = new yaml.Type(
+    dummyActionConfigDict.cleanCheckout.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && "repoSetupName" in data
-            && isString(data.repoSetupName)
-            && "commitish" in data
-            && isString(data.commitish);
-    },
+        resolve: function(data) {
+            return data !== null
+                && "repoSetupName" in data
+                && isString(data.repoSetupName)
+                && "commitish" in data
+                && isString(data.commitish);
+        },
 
-    construct: function(data) {
-        return new actionConfig.CleanCheckoutAction(
-            data.repoSetupName,
-            data.commitish
-        );
-    },
+        construct: function(data) {
+            return new actionConfig.CleanCheckoutAction(
+                data.repoSetupName,
+                data.commitish
+            );
+        },
 
-    instanceOf: actionConfig.CleanCheckoutAction
-});
+        instanceOf: actionConfig.CleanCheckoutAction
+    }
+);
 
-let gitCommandActionType = new yaml.Type("!git", {
-    kind: "mapping",
+let gitCommandActionType = new yaml.Type(
+    dummyActionConfigDict.gitCommand.tag, 
+    {
+        kind: "mapping",
 
-    resolve: function(data) {
-        return data !== null
-            && "repoSetupName" in data
-            && isString(data.repoSetupName)
-            && "command" in data
-            && isString(data.command);
-    },
+        resolve: function(data) {
+            return data !== null
+                && "repoSetupName" in data
+                && isString(data.repoSetupName)
+                && "command" in data
+                && isString(data.command);
+        },
 
-    construct: function(data) {
-        return new actionConfig.GitCommandAction(
-            data.repoSetupName,
-            data.command,
-            data.arguments || []
-        );
-    },
+        construct: function(data) {
+            return new actionConfig.GitCommandAction(
+                data.repoSetupName,
+                data.command,
+                data.arguments || []
+            );
+        },
 
-    instanceOf: actionConfig.GitCommandAction,
-});
+        instanceOf: actionConfig.GitCommandAction,
+    }
+);
 
 let schema = yaml.Schema.create(upstream.LEVEL_CONFIG_SCHEMA, [
     unstageActionType,
