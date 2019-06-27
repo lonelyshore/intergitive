26b3c5859d341f3e79a03e541632da6e74a577af
zedaizd <test@test.server>
2019-06-27 08:10:33 +0800

    Store loads in repository store setups of a level

--- paths.js
+++ paths.js
@@ -2,9 +2,12 @@ const path = require('path');
 const runAndTestBasePath = path.resolve(__dirname, './example');
 
 const runAndTest = {
-    RESOURCES_PATH: path.join(runAndTestBasePath, "resources"),
+    RESOURCES_PATH: path.join(runAndTestBasePath, 'resources'),
     BUNDLE_PATH: [],
     STATIC_PATH: path.join(path.relative(__dirname, runAndTestBasePath), 'static'),
+    PLAYGROUND_PATH: path.join(runAndTestBasePath, 'playground'),
+    REF_STORE_PATH: path.join(runAndTestBasePath, 'repoStore', 'refs'),
+    CHECKPOINT_STORE_PATH: path.join(runAndTestBasePath, 'repoStore', 'checkpoints')
 };
 
 const result = runAndTest;
@@ -12,3 +15,6 @@ const result = runAndTest;
 module.exports.RESOURCES_PATH = result.RESOURCES_PATH;
 module.exports.BUNDLE_PATH = result.BUNDLE_PATH;
 module.exports.STATIC_PATH = result.STATIC_PATH;
+module.exports.PLAYGROUND_PATH = result.PLAYGROUND_PATH;
+module.exports.REF_STORE_PATH = result.REF_STORE_PATH;
+module.exports.CHECKPOINT_STORE_PATH = result.CHECKPOINT_STORE_PATH;
