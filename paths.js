0f9673a3107bb4ec22ce96d708012a679b964319
zedaizd <test@test.server>
2019-07-25 09:02:02 +0800

    Adjust verifyInput appearance; Add common assets in store

--- paths.js
+++ paths.js
@@ -3,13 +3,13 @@ const readonly = require('./lib/readonly');
 const runAndTestBasePath = path.resolve(__dirname, './example');
 
 let baseSetting = {
+    PROJECT_DIR: __dirname,
     RESOURCES_PATH: path.join(runAndTestBasePath, 'resources'),
     BUNDLE_PATH: [],
-    STATIC_PATH: path.join(path.relative(__dirname, runAndTestBasePath), 'static'),
     PLAYGROUND_PATH: path.join(runAndTestBasePath, 'playground'),
     REPO_STORE_COLLECTION_NAME: 'repo-stores',
 };
 
 baseSetting = readonly.wrap(baseSetting);
 
-exports = module.exports = baseSetting;
+module.exports = baseSetting;
