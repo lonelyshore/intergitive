428754aa2a56c06deea679160112518b3686236c
zedaizd <test@test.server>
2019-10-31 09:10:51 +0800

    Add VerifyAllRepoStep to verify all repositories at once

--- test/tests/level-schema.js
+++ test/tests/level-schema.js
@@ -11,7 +11,7 @@ chai.should();
 describe('Load Level Config', function() {
     it('Production can recognize all dev steps', function() {
         let prodSchema = require('../../lib/level-config-schema').LEVEL_CONFIG_SCHEMA;
-        let devSchema = require('../../dev/config-schema').LEVEL_CONFIG_SCHEMA;
+        let devSchema = require('../../dev/level-config-schema').LEVEL_CONFIG_SCHEMA;
 
         Object.keys(prodSchema.compiledTypeMap.mapping).should.contain.members(
             Object.keys(devSchema.compiledTypeMap.mapping)
