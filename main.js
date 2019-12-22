719922bc7977d50b170cbb0f56864b82a1a5bc4d
zedaizd <test@test.server>
2019-12-22 22:51:53 +0800

    Correct the but that the process is not killed when its window is closed

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const { app, BrowserWindow } = require('electron')
 function createWindow () {
   // Create the browser window.
   let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 1000,
     webPreferences: {
       nodeIntegration: true
     }
@@ -15,7 +15,19 @@ function createWindow () {
   // and load the index.html of the app.
   win.loadFile('index.html')
   win.webContents.openDevTools();
+
+  win.on('closed', () => {
+    win = null;
+  })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  // 在 macOS 中，一般會讓應用程式及選單列繼續留著，
+  // 除非使用者按了 Cmd + Q 確定終止它們
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
