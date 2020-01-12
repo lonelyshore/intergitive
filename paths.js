a858ea976ebeea102e2cee7d098e4addfe543a3d
zedaizd <test@test.server>
2020-01-12 20:34:07 +0800

    Allow to switch between courses

--- paths.js
+++ paths.js
@@ -5,9 +5,11 @@ const runAndTestBasePath = path.resolve(__dirname, './example');
 let baseSetting = {
     PROJECT_DIR: __dirname,
     RESOURCES_PATH: path.join(runAndTestBasePath, 'resources'),
+    COURSE_RESOURCES_PATH: path.join(runAndTestBasePath, 'course-resources'),
     BUNDLE_PATH: [],
     PLAYGROUND_PATH: path.join(runAndTestBasePath, 'playground'),
     REPO_STORE_COLLECTION_NAME: 'repo-stores',
+    COURSE: 'demo'
 };
 
 baseSetting = readonly.wrap(baseSetting);
