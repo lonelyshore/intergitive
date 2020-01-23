f7fc228033d7252e707ec4c351af9d06f7ce9f2e
zedaizd <test@test.server>
2020-01-23 21:33:20 +0800

    Refactor: Extract RunTimeCourseSettings

--- paths.js
+++ paths.js
@@ -1,21 +1,8 @@
-const path = require('path');
-const readonly = require('./lib/readonly');
-const CourseStruct = require('./lib/course-struct');
+const RuntimeCourseSettings = require('./lib/runtime-course-settings');
 
-
-let exampleStruct = new CourseStruct('./example');
-
-let baseSetting = {
-    BASE_PATH: exampleStruct.basePath,
-    PROJECT_DIR: __dirname,
-    RESOURCES_PATH: exampleStruct.resourcesPath,
-    COURSE_RESOURCES_PATH: exampleStruct.courseResourcesPath,
-    BUNDLE_PATH: [],
-    PLAYGROUND_PATH: exampleStruct.playgroundPath,
-    REPO_STORE_COLLECTION_NAME: exampleStruct.repoStoreCollectionName,
-    COURSE: 'fork'
-};
-
-baseSetting = readonly.wrap(baseSetting);
+let baseSetting = new RuntimeCourseSettings(
+    __dirname,
+    './example'
+);
 
 module.exports = baseSetting;
