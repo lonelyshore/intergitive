693cb6814f4a91f95609fc37d42de803ef8d823a
zedaizd <test@test.server>
2020-01-23 15:49:25 +0800

    Refactor paths

--- paths.js
+++ paths.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const readonly = require('./lib/readonly');
-const runAndTestBasePath = path.resolve(__dirname, './example');
+const CourseStruct = require('./lib/course-struct');
+
+
+let exampleStruct = new CourseStruct('./example');
 
 let baseSetting = {
+    BASE_PATH: exampleStruct.basePath,
     PROJECT_DIR: __dirname,
-    RESOURCES_PATH: path.join(runAndTestBasePath, 'resources'),
-    COURSE_RESOURCES_PATH: path.join(runAndTestBasePath, 'course-resources'),
+    RESOURCES_PATH: exampleStruct.resourcesPath,
+    COURSE_RESOURCES_PATH: exampleStruct.courseResourcesPath,
     BUNDLE_PATH: [],
-    PLAYGROUND_PATH: path.join(runAndTestBasePath, 'playground'),
-    REPO_STORE_COLLECTION_NAME: 'repo-stores',
+    PLAYGROUND_PATH: exampleStruct.playgroundPath,
+    REPO_STORE_COLLECTION_NAME: exampleStruct.repoStoreCollectionName,
     COURSE: 'fork'
 };
 
