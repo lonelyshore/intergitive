00115c0fc742c84c6c8e55a44e8fc32419109cbc
zedaizd <test@test.server>
2020-01-26 13:13:18 +0800

    Move several settings in to yaml configuration files

--- paths.js
+++ paths.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
 const RuntimeCourseSettings = require('./lib/runtime-course-settings');
+
+let rawSetting = fs.readFileSync('example-course-settings.yaml');
+let courseSettings = yaml.safeLoad(rawSetting);
 
 let baseSetting = new RuntimeCourseSettings(
     __dirname,
-    './example'
+    courseSettings
 );
 
 module.exports = baseSetting;
