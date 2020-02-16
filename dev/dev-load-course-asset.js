bc395419cdd4af880855cb82be455805eb2c8cf4
zedaizd <test@test.server>
2020-02-16 16:32:45 +0800

    Loads dev actions in course-validity checks

---
+++ dev/dev-load-course-asset.js
@@ -0,0 +1,33 @@
+const yaml = require('js-yaml');
+const loadCourseAsset = require('../lib/load-course-asset');
+const LEVEL_SCHEMA = require('../dev/level-config-schema').LEVEL_CONFIG_SCHEMA;
+
+class DevLoaderPair extends loadCourseAsset.LoaderPair {
+
+    /**
+     * 
+     * @param {AssetLoader} globalLoader 
+     * @param {AssetLoader} courseLoader 
+     */
+    constructor(globalLoader, courseLoader) {
+        super(globalLoader, courseLoader);
+    }
+
+    loadLevelFromCourse(levelName, courseName) {
+        let loader = this.getCourseLoader(courseName);
+
+        return loader.loadTextContent(levelName)
+        .then(text => {
+            console.log(`loading ${levelName} json`);
+            return yaml.safeLoad(text, { schema: LEVEL_SCHEMA });
+        });            
+    }
+}
+
+module.exports = Object.assign(module.exports, loadCourseAsset);
+module.exports.createCourseAssetLoaderPair = (courseSettings) => {
+    let loaderPair = loadCourseAsset.createCourseAssetLoaderPair(
+        courseSettings
+    );
+    return new DevLoaderPair(loaderPair.global, loaderPair.course);
+};
