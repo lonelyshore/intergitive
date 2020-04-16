6369ec24399b36248d77fc66189ecae96e1c1c42
zedaizd <test@test.server>
2020-04-16 22:42:29 +0800

    Correct initial setting path; ignore bake folder when building

--- paths.js
+++ paths.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 const yaml = require('js-yaml');
 
 const RuntimeCourseSettings = require('./lib/runtime-course-settings');
 
-let rawSetting = fs.readFileSync('example-course-settings.yaml');
+let rawSetting = fs.readFileSync(path.join(__dirname, 'example-course-settings.yaml'));
 let courseSettings = yaml.safeLoad(rawSetting);
 
 let baseSetting = new RuntimeCourseSettings(
