fbae5a5da13e7631f95f97f589a58bc01cb793a6
zedaizd <zedaizd@gmail.com>
2021-02-14 10:27:48 +0800

    Copy index.html in webpack.config; Add node-loader to load .node files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const path = require('path');
-
+const fs = require('fs-extra');
 const distPath = path.resolve(__dirname, 'dist');
 
 const main = {
@@ -10,17 +10,32 @@ const main = {
     filename: 'main.js',
     path: distPath,
   },
-  target: 'electron-main'
+  target: 'electron-main',
+  node: {
+    __dirname: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.node$/,
+        loader: 'node-loader',
+      },
+    ],
+  },
+  //mode: "development"
 };
 
 const renderer = {
   entry: path.resolve(__dirname, './lib/render-level.js'),
   output: {
     filename: 'render-level.js',
-    path: distPath,
+    path: path.join(distPath, 'lib'),
   },
   target: 'electron-renderer'
 };
 
 
 module.exports = [main, renderer];
+
+fs.ensureDir(path.resolve(__dirname, 'dist'));
+fs.copyFileSync(path.resolve(__dirname, 'index.html'), path.resolve(__dirname, 'dist/index.html'));
